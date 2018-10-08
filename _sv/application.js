ab97410e9e7a8b245272056b17104d15ea37eabe
jheftmann <jheftmann@mac.com>
2018-10-08 13:17:54 -0400

    add bootstrap and configure mobile nav

--- _sv/application.js
+++ _sv/application.js
@@ -5,6 +5,12 @@
 
 !function($) {
 
+  // open/close mobile navigation
+  $('#sv-toggle-nav.sv-ui-expansion-is-closed').click(function(){
+    event.preventDefault();
+    $('.sv-navigation').addClass('sv-navigation-is-open');
+  })
+
 }(window.jQuery);
 
 $(window).load(function(){
